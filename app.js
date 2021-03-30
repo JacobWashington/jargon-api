70d274f66471dfc24c459fbcc45a0e99c7c8d3c4
jacobwashington <jacobwashington.dev@gmail.com>
2021-03-29 22:34:40 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+// Imports
+require("dotenv").config();
+const express = require("express");
+const routes = require("./routes");
+const cors = require("cors");
+const passport = require("passport");
+require("./config/passport")(passport);
+
+// App Set Up
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+app.use(passport.initialize());
+
+// API Routes
+app.get('/jargon/', (req, res) => {
+  res.json({message: "Welcome to jargon"});
+});
+
+// Server
+const server = app.listen(PORT, () =>
+  console.log(`Server is running on PORT: ${PORT}`)
+);
+
+module.exports = server;
